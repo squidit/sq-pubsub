0a3628d48e45976db2c84b9ef75a99bbfb61815a
Lucas Santos <lhs.santoss@gmail.com>
2017-10-19 11:35:01 -0200

    📝 Commenting JSDocs on code

--- src/sq-pubsub.js
+++ src/sq-pubsub.js
@@ -1,6 +1,22 @@
 const PubSub = require('@google-cloud/pubsub')
 
+/**
+ * Main pubsub class
+ *
+ * @public
+ * @param {String} GceProjectName Project name on GCE
+ * @param {String} GceKeyFileName Project JSON credentials file path
+ * @class
+ */
 class SQPubSub {
+  /**
+   * Creates a new instance of pubsub
+   *
+   * @param {String} GceProjectName Project name on GCE
+   * @param {String} GceKeyFileName Project JSON credentials file path
+   * @constructor
+   * @see {@link https://github.com/squidit/sq-pubsub#using|Documentation}
+   */
   constructor (GceProjectName, GceKeyFileName) {
     this._pubsub = PubSub({
       projectId: GceProjectName,
@@ -8,6 +24,16 @@ class SQPubSub {
     })
   }
 
+  /**
+   * Listen to messages on a particular subscription
+   *
+   * @param {String} subscriptionName Name of the subscription
+   * @param {Function} cb Callback function with the signature: `cb(message, err)`
+   * @param {bool} autoAck Should the message be acknowledged upon receival? Defaults to false
+   * @see {@link https://github.com/squidit/sq-pubsub#listening-to-a-subscription|Documentation}
+   * @see {@link https://github.com/squidit/sq-pubsub#autoack|AutoAck Documentation}
+   * @public
+   */
   listenMessages (subscriptionName, cb, autoAck = false) {
     console.log(`Listening ${subscriptionName}...`)
     let subscription = this._pubsub.subscription(subscriptionName)
@@ -25,22 +51,47 @@ class SQPubSub {
     return subscription
   }
 
+  /**
+   * Publishes a new message to a topic
+   * @param {String} topicName Topic name on Pubsub
+   * @param {any} data Data to be sent
+   * @see {@link https://github.com/squidit/sq-pubsub#publish-a-message|Documentation}
+   * @public
+   */
   publishMessage (topicName, data) {
     const publisher = this._pubsub.topic(topicName).publisher()
     return publisher.publish(Buffer.from(JSON.stringify(data)))
   }
 
+  /**
+   * Removes all listeners from a subscription
+   * @param {Object} subscription Subscription object returned by ListenMessages
+   * @public
+   * @see {@link https://github.com/squidit/sq-pubsub#unlistening-a-subscription|Documentation}
+   */
   unlisten (subscription) {
     subscription.removeListener('message', subscription._events.message)
     subscription.removeListener('error', subscription._events.error)
     return this
   }
 
+  /**
+   * Acknowledges a message as received
+   * @param {Object} message Message object returned by the callback
+   * @public
+   * @see {@link https://github.com/squidit/sq-pubsub#ack-or-nack-a-message|Documentation}
+   */
   ack (message) {
     console.log(`Message #${message.id} acknowledged with AckID ${message.ackId}`)
     return message.ack()
   }
 
+  /**
+   * Not acknowledges a message as received
+   * @param {Object} message Message object returned by the callback
+   * @public
+   * @see {@link https://github.com/squidit/sq-pubsub#ack-or-nack-a-message|Documentation}
+   */
   nack (message) {
     console.log(`Message #${message.id} has been marked as not acknowledged by the user`)
     return message.nack()
